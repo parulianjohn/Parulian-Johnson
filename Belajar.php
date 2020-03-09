2ed3fdc0e79cb71ce21aa5528b0b4d21055cfe46
parulianjohn <61967182+parulianjohn@users.noreply.github.com>
2020-03-09 16:37:24 +0700

    belajar.php

---
+++ Belajar.php
@@ -0,0 +1,12 @@
+<?php
+class coba extends CI_Controller
+{
+pubic function index()
+{
+echo "ini adalah controller coba"'
+}
+public function detail ($nama)
+{
+echo "Selamat Datang: ".$nama;
+}
+}
